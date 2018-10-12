9c32e6e2ff90b92e053e793e94617ee988b3ee86
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-10-12 17:18:30 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -15,7 +15,7 @@ client.on('message', async (message, member) => {
 	
   if (message.content.startsWith(`${prefix}hello`)) {
 	let helloEmbed = new Discord.RichEmbed()
-	.setAuthor(`Hello ${message.author.tag}!`) 
+	.setAuthor(`Hello ${message.mentions.tag}!`) 
 	.setDescription(`"Hello member, how are you doing today?`)
 	.setColor("#BA55D3")  
 	.setTimestamp();
