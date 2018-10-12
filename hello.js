89cf20f6dd7632161a4fedaaffe2e69c7fd10e2f
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-10-12 16:27:43 -0400

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const request = require('snekfetch');
+const config = require('./botconfig.json');
+const { prefix, token } = require('./botconfig.json');
+
+client.on(`ready`, () => {
+  console.log(`I am ready to say hello to the world.`);
+client.user.setActivity('HelloDawn: Episode 1');
+
+});
+
+client.login(process.env.BOT_TOKEN);
