98ea28a3ab260ce06a795245a0938b8d90599441
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-10-20 17:14:06 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,10 +3,12 @@ const client = new Discord.Client();
 const request = require('snekfetch');
 const config = require('./botconfig.json');
 const { prefix, token } = require('./botconfig.json');
+const moment = require('moment');
+require('moment-duration-format');
 
 client.on(`ready`, () => {
-  console.log(`I am ready to say hello to the world.`);
-client.user.setActivity("King's Raid OST", {type: "LISTENING"});
+  console.log(`Feminism Is POWER!`);
+client.user.setActivity("**To All Sides**", {type: "LISTENING"});
 
 });
 
@@ -14,10 +16,12 @@ client.on('message', async (message, member) => {
   	   let user = message.mentions.users.first(); 
 	
   if (message.content.startsWith(`${prefix}hello`)) {
+	 let replies = ["Hello ${member.author.tag}! I hope you are enjoying your time here. 😄", "Hello! How are you? 😄", "Hello! I hope you are behaving yourself. 😄", "Hello! Staying out of trouble I hope. 😄", "Hello!"];
+let result = Math.floor((Math.random() * replies.length));
 	let helloEmbed = new Discord.RichEmbed()
-	.setAuthor(`Hello ${message.author.tag}!`) 
-	.setDescription(`Hello member, how are you doing today?`)
-	.setColor("#BA55D3")  
+	.setDescription(replies[result])
+	.setColor("#FFC0CB") 
+        .setFooter(`Said Hello To ${user.tag}`)
 	.setTimestamp();
 	  message.channel.send(helloEmbed);
 }
@@ -31,7 +35,7 @@ if(!user) return message.channel.send("You didn't mention anyone..?");
     let avatarEmbed = new Discord.RichEmbed()
     .setAuthor(`${user.username}'s Profile Picture`)
     .setImage(user.displayAvatarURL)
-    .setColor("#BA55D3");
+    .setColor("#FFC0CB");
     message.channel.send(avatarEmbed);
 }
   
