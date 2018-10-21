0f81e94e4e89a7c9f17e4600c134e40764179a9f
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-10-20 21:43:40 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -183,6 +183,31 @@ let result = Math.floor((Math.random() * replies.length));
     .setTimestamp();
     return message.channel.send(feministembed);
 }	
+	
+	let xp = require("./xp.json");
+	let xpAdd = Math.floor(Math.random() * 7) + 8;
+console.log(xpAdd);
+
+if(!xp[message.author.id]) {
+  xp[message.author.id] = {
+    xp: 0,
+    level: 1
+  };
+}
+
+
+let curxp = xp[message.author.id].xp;
+let curlvl = xp[message.author.id].level;
+let nxtLvl = xp[message.author.id].level * 300;
+xp[message.author.id].xp =  curxp + xpAdd;
+if(nxtLvl <= xp[message.author.id].xp){
+  xp[message.author.id].level = curlvl + 1;
+  let lvlup = new Discord.RichEmbed()
+  .setTitle("You've leveled up! Keep up with the conversations! :tada:")
+  .setColor("#FFC0CB")
+  .addField("New Level!", curlvl + 1);
+  message.channel.send(lvlup).then(message => {message.delete(5000)})
+}
 });
 	client.on('guildMemberAdd', (member) => {
   let user = member.user.tag;
